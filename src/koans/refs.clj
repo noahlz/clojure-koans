99f46eaee5e6ba8c79b62d1b360a0232ff1a76fe
noahlz <nzucker@gmail.com>
2011-12-15 00:23:19 -0500

    completed refs

--- src/koans/refs.clj
+++ src/koans/refs.clj
@@ -3,18 +3,18 @@
 
 (meditations
   "In the beginning, there was a word"
-  (= __ (deref the-world))
+  (= @the-world (deref the-world))
 
   "You can get the word more succinctly, but it's the same"
-  (= __ @the-world)
+  (= (deref the-world) @the-world)
 
   "You can be the change you wish to see in the world."
-  (= __ (do
-          (dosync (ref-set the-world "better"))
-          @the-world))
+  (= "better" (do
+                (dosync (ref-set the-world "better"))
+                @the-world))
 
   "Alter where you need not replace"
-  (= __ (let [exclamator (fn [x] (str x "!"))]
+  (= "better!!!"  (let [exclamator (fn [x] (str x "!"))]
           (dosync
            (alter the-world exclamator)
            (alter the-world exclamator)
@@ -22,13 +22,12 @@
           @the-world))
 
   "Don't forget to do your work in a transaction!"
-  (= 0 (do __
-           @the-world))
+  (= 0 (dosync (ref-set the-world 0)))
 
   "Functions passed to alter may depend on the data in the ref"
   (= 20 (do
-          (dosync (alter the-world ___))))
-
+          (dosync (alter the-world #(+ % 20)))))
+  
   "Two worlds are better than one"
   (= ["Real Jerry" "Bizarro Jerry"]
        (do
@@ -36,4 +35,5 @@
           (ref-set the-world {})
           (alter the-world assoc :jerry "Real Jerry")
           (alter bizarro-world assoc :jerry "Bizarro Jerry")
-          __))))
+          [(:jerry @the-world) (:jerry @bizarro-world)]))))
+
